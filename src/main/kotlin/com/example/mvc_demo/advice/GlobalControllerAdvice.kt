45c65d3172b03022a97b636fef6ca95b13f1fa92
jhunderdog <old222@naver.com>
2023-06-04 07:06:23 +0900

    ExceptionHandler with ErrorResponse Class

--- src/main/kotlin/com/example/mvc_demo/advice/GlobalControllerAdvice.kt
+++ src/main/kotlin/com/example/mvc_demo/advice/GlobalControllerAdvice.kt
@@ -6,10 +6,9 @@ import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.RestControllerAdvice
-import java.lang.RuntimeException
 
 
-@RestControllerAdvice
+//@RestControllerAdvice
 class GlobalControllerAdvice {
     @ExceptionHandler(value = [RuntimeException::class])
     fun exception(e: RuntimeException): String {
--- src/main/kotlin/com/example/mvc_demo/controller/exception/ExceptionApiController.kt
+++ src/main/kotlin/com/example/mvc_demo/controller/exception/ExceptionApiController.kt
@@ -1,25 +1,116 @@
 package com.example.mvc_demo.controller.exception
 
+import com.example.mvc_demo.model.http.Error
+import com.example.mvc_demo.model.http.ErrorResponse
+import com.example.mvc_demo.model.http.UserRequest
+import jakarta.servlet.http.HttpServletRequest
+import jakarta.validation.ConstraintViolationException
+import jakarta.validation.Valid
+import jakarta.validation.constraints.Min
+import jakarta.validation.constraints.NotBlank
+import jakarta.validation.constraints.Size
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
+import org.springframework.validation.BindingResult
+import org.springframework.validation.FieldError
+import org.springframework.validation.annotation.Validated
+import org.springframework.web.bind.MethodArgumentNotValidException
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
+import java.time.LocalDateTime
 
 @RestController
 @RequestMapping("/api/exception")
+@Validated
 class ExceptionApiController {
+    @GetMapping("")
+    fun getMapping(
+        @NotBlank
+        @Size(min = 2, max = 6)
+        @RequestParam(value = "name") name: String,
+        @jakarta.validation.constraints.NotNull(message = "age값이 누락되었습니다.")
+        @Min(value = 20, message = "age는 20보다 커야 합니다.")
+        @RequestParam(name = "age") age: Int
+    ): String {
+        println(name)
+        println(age)
+        return  name + " " + age
+    }
 
+    @PostMapping
+    fun post(@Valid @RequestBody userRequest: UserRequest): UserRequest {
+        println(userRequest)
+        return userRequest
+    }
+
+    @ExceptionHandler(value= [MethodArgumentNotValidException::class])
+    fun methodArgumentNotValidException(e: MethodArgumentNotValidException, request: HttpServletRequest): ResponseEntity<ErrorResponse> {
+        val errors = mutableListOf<Error>()
+        e.bindingResult.allErrors.forEach { errorObject ->
+            val error = Error().apply {
+                this.field = (errorObject as FieldError).field
+                this.message = errorObject.defaultMessage
+                this.value = errorObject.rejectedValue
+            }
+
+            errors.add(error)
+        }
+        val errorResponse = ErrorResponse().apply {
+            this.resultCode = "FAIL"
+            this.httpStatus = HttpStatus.BAD_REQUEST.value().toString()
+            this.httpMethod = request.method
+            this.message = "요청에 에러가 발생하였습니다."
+            this.path = request.requestURI.toString()
+            this.timestamp = LocalDateTime.now()
+            this.errors =errors
+        }
+        // 3. ResponseEntity
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse)
+    }
+
+    @ExceptionHandler(value = [ConstraintViolationException::class])
+    fun constraintViolationException(e: ConstraintViolationException, request: HttpServletRequest) : ResponseEntity<ErrorResponse>{
+        // 1. 에러 분석
+        val errors = mutableListOf<Error>()
+        e.constraintViolations.forEach {
+            val error = Error().apply {
+                this.field = it.propertyPath.last().name
+                this.message = it.message
+                this.value = it.invalidValue
+            }
+            errors.add(error)
+        }
+        // 2. ErrorResponse
+        val errorResponse = ErrorResponse().apply {
+            this.resultCode = "FAIL"
+            this.httpStatus = HttpStatus.BAD_REQUEST.value().toString()
+            this.httpMethod = request.method
+            this.message = "요청에 에러가 발생하였습니다."
+            this.path = request.requestURI.toString()
+            this.timestamp = LocalDateTime.now()
+            this.errors =errors
+        }
+        // 3. ResponseEntity
+    return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse)
+
+    }
     @GetMapping("/hello")
     fun hello(){
         val list = mutableListOf<String>()
         val temp = list[0]
     }
 
-    @ExceptionHandler(value = [IndexOutOfBoundsException::class])
-    fun indexOutOfBoundsException(): ResponseEntity<String> {
-        println("controller exception handler")
-        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Index Error")
-    }
+
+
+
+//    @ExceptionHandler(value = [IndexOutOfBoundsException::class])
+//    fun indexOutOfBoundsException(): ResponseEntity<String> {
+//        println("controller exception handler")
+//        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Index Error")
+//    }
 }
